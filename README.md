431677818a9a466f8c57e4be92106812a4a29edb
Luiza Fortes <luizafortes@gmail.com>
2020-09-29 10:40:19 -0300

    Publicando os dados dos sensores no Mqtt-Broker e outras alterações descritas abaixo:
    - Alteração dos nomes das pastas dos Mqtt-clients

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # smartcare-iot
 ### Este repositório contém o desenvolvimento do projeto SmartCare Environment - Unilasalle-RJ
    ![Planta baixa com a marcação dos sensores](/img/planta_baixa_sensores.png)
+
 ### Componentes do projeto
 
-- Raspberry Pi 3 B+
-- Wemos D1 mini Esp8266
-- Sensores: 
-    - PIR
-    - Ultrassônico
-    - Peso
-- Atuadores:
-    - Leds
+- Raspberry Pi 3 B+ como Servidor Mqtt-Broker
+- Maquina Virtual como C-Mqtt-client
+- D1-mini-lite(Esp8266) como Python-Mqtt-client
+    - Sensores: 
+        - PIR
+        - Ultrassônico
+        - Peso
+    - Atuadores:
+        - Leds
+
+### Servidor Mqtt-Broker
+- Responsável por receber publicação dados dos sensores em tópicos e enviar esses dados para quem se inscreveu nesses tópicos.
+
+### C-Mqtt-client
+- Responsável pela leitura e envio dos dados dos sensores conectados a ele para o Mqtt-Broker, e por escutar os tópicos aos quais se inscreveu e acionar os atuadores caso seja solicitado.
+
+### Maquina Virtual Python-Mqtt-client
+- Responsável por escutar todos os tópicos do Mqtt-Broker e salvar os dados no banco de dados
--- c-client/src/main.cpp
+++ c-client/src/main.cpp
@@ -2,42 +2,46 @@
 #include<Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <Ultrasonic.h>
 #include <string.h>
+#include <Ultrasonic.h>
 
 #include <MQTTClient.h>
 // Update these with values suitable for your network.
 
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "NET-WIFI-907";
+const char* password = "tekbond793";
 const char* mqtt_server = "192.168.0.74";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
 String clientId = "D1 mini";
 String clientUser = "smartcare";
 String clientPass = "unilasalle";
 long lastMsg = 0;
 char msg[50];
 int value = 0;
-
+String strClienteId(clientId);
 
 void publicar(const char* topic, String mensagem, boolean retained);
 void on_message(char* topic, byte* payload, unsigned int length);
-void verificaIdosoDeitado();
+//void verificaIdosoDeitado();
 void  verificaUltrasonico();
 void  verificaPresenca();
 
 #define pinoPIRQuarto 13 // D7 PIR do quarto
 int statusPIRQuarto; //Variavel para guardar valor do sensor do quarto
 
+/*HCSR04(trigger, echo, temperature, distance)*/
+
+
+
 #define pinoTriggerQuarto 14 // D5
 #define pinoEchoQuarto 12 // D6
-Ultrasonic ultrasonic(pinoTriggerQuarto, pinoEchoQuarto);
-int distance;
+Ultrasonic ultrasonic(pinoTriggerQuarto, pinoEchoQuarto, 30000); // (Trig PIN,Echo PIN)
 
-#define pinoButtonQuarto   15 // D8 BOTAO
-#define pinoLedVermelho  4 // D2 LED VERMELHO
+//#define pinoButtonQuarto   15 // D8 BOTAO
+//#define pinoLedVermelho  4 // D2 LED VERMELHO
 #define pinoLedAmarelo  2 // D4 LED AMARELO
 #define pinoLedQuarto  5 // D1 LED BRANCO
 
@@ -75,9 +79,9 @@ void reconnect() {
     if (client.connect(clientId.c_str(), clientUser.c_str(), clientPass.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
+      client.publish("home/quarto/dispositivos", clientId.c_str());
       // ... and resubscribe
-      client.subscribe("quarto/#");
+      client.subscribe("home/#");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -92,9 +96,9 @@ void reconnect() {
 void setup() {  
 
   pinMode(pinoPIRQuarto, INPUT); //Define pino do sensor de presença como entrada  
-  pinMode(pinoButtonQuarto, INPUT);
+  //pinMode(pinoButtonQuarto, INPUT);
   pinMode(pinoLedQuarto, OUTPUT); //DEFINE O PINO do led COMO SAÍDA
-  pinMode(pinoLedVermelho, OUTPUT); //DEFINE O PINO do led COMO SAÍDA
+  //pinMode(pinoLedVermelho, OUTPUT); //DEFINE O PINO do led COMO SAÍDA
   pinMode(pinoLedAmarelo, OUTPUT); //DEFINE O PINO do led COMO SAÍDA  
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
 
@@ -110,8 +114,9 @@ void loop() {
     reconnect();
   }
   
-  verificaIdosoDeitado();
+  //verificaIdosoDeitado();
   verificaUltrasonico();
+  
   verificaPresenca();
 
   client.loop();
@@ -127,19 +132,36 @@ void publicar(const char* topic, String mensagem, boolean retained)
 }
 
 void on_message(char* topic, byte* payload, unsigned int length) {
+
+  
   char msg[length];
-  Serial.print("Message arrived [");
+  Serial.print("Message arrived on topic: ");  
+  Serial.print("[ ");
   Serial.print(topic);
-  Serial.print("] ");
+  Serial.println(" ] ");
+
+  Serial.print("payload: ");
   for (int i = 0; i < length; i++) {
     msg[i] = (char)payload[i];
     Serial.print((char)payload[i]);
   }
-
-  Serial.println();
+  // Serial.println("");
+  // Serial.print("msg: [ ");
+  // Serial.print(msg);
+  // Serial.println(" ]");
+  
   String msgStr(msg);
   // Find out how many bottles we should generate lyrics for
   String topicStr(topic);
+
+  Serial.print("msgStr = ");  
+  Serial.print("[ ");
+  Serial.print(msgStr);
+  Serial.print(" ] : to -> ");
+  msgStr.remove(5, msgStr.indexOf('/'));
+  Serial.println(msgStr);
+  delay(3000);
+
 
   if (topicStr.indexOf('/') >= 0) {
     // The topic includes a '/', we'll try to read the number of bottles from just after that
@@ -147,80 +169,124 @@ void on_message(char* topic, byte* payload, unsigned int length) {
     // Now see if there's a number of bottles after the '/'
   }  
   
-  int topicValue;
-  if (topicStr.compareTo("quarto/cama"))
+  char topicValue;
+
+  Serial.print("strcmp = ");  
+  Serial.print("[ ");
+  Serial.print(strncmp(topic, "home/quarto/sensor/PIR-1", strlen(topic)));
+  Serial.println(" ] ");
+
+  Serial.print("topicStr.compareTo = ");  
+  Serial.print("[ ");
+  Serial.print(topicStr.compareTo("home/quarto/sensor/UIR-1"));
+  Serial.println(" ] ");
+
+  if (strncmp(topic, "home/quarto/sensor/BTN-1", strlen(topic)) == 0)
   {
-    topicValue = 1;
+    Serial.println("Dormindo.");
+  }
+  else if (strncmp(topic, "home/quarto/sensor/UIR-1", strlen(topic)) == 0)
+  {    
+    Serial.print("porta: ");
+    Serial.println(msg);
+  }
+  else if (strncmp(topic, "home/quarto/sensor/PIR-1", strlen(topic)) == 0)
+  {
+    Serial.println("Tem gente.");
+    digitalWrite(pinoLedQuarto, HIGH);
+    delay(1000);    
+    digitalWrite(pinoLedQuarto, LOW);
   }
   else
   {
-    if (topicStr.compareTo("quarto/porta"))
-    {
-      topicValue = 2;
-    }
-    else
-    {
-      if (topicStr.compareTo("quarto/teto"))
-      {
-        topicValue = 3;
-      }
-      else
-      {
-        Serial.println("Tópico não identificado.");
-      }
-    }
+    Serial.println("Tópico não identificado.");
   }
   
   switch (topicValue) {
   case 1:  //cama
-
+    if (msg == "1")
+    {
+      Serial.print(topic);
+      Serial.print(": ");
+      Serial.println(msg);
+      //digitalWrite(pinoLedAmarelo, HIGH);
+    }else
+    {
+      //digitalWrite(pinoLedAmarelo, LOW);
+    }   
     break;
+
   case 2: //porta
-    
+    if (msg == "1")
+    {
+      Serial.print(topic);
+      Serial.print(": ");
+      Serial.println(msg);
+      digitalWrite(pinoLedAmarelo, HIGH);
+    }else
+    {
+      digitalWrite(pinoLedAmarelo, LOW);
+    }   
     break;
 
-  case 3: //teto
-
+  case 3: //teto   
+    if (msg == "1")
+    {
+      Serial.print(topic);
+      Serial.print(": ");
+      Serial.println(msg);
+      digitalWrite(pinoLedQuarto, HIGH);
+    }else
+    {
+       digitalWrite(pinoLedQuarto, LOW);
+    }     
     break;
     
   default:      
     break;
   };
+
 }
 
-void verificaUltrasonico() {  
-  float distancia = ultrasonic.read(); 
-  Serial.println(distancia);
-
-  if (distancia < 20)
-  {
-    digitalWrite(pinoLedAmarelo, HIGH);
-    client.publish("home/quarto/sensor/UIR-1", "1");
-  } else {
-    digitalWrite(pinoLedAmarelo, LOW);
-  }  
-}
-
-void verificaIdosoDeitado(){
-  int sensorVal = digitalRead(pinoButtonQuarto);  
-  if (sensorVal == HIGH) {
-    digitalWrite(pinoLedVermelho, LOW);
-    Serial.println("O idoso esta deitado");
-  } else {
-    digitalWrite(pinoLedVermelho, HIGH);
-    client.publish("home/quarto/sensor/BTN-1", "1");
-    Serial.println("O idoso nao esta deitado");
-  }
-}
 
 void verificaPresenca(){
   int sensorVal = digitalRead(pinoPIRQuarto);  
-  if (sensorVal == HIGH) {
-    digitalWrite(pinoLedQuarto, HIGH);    
-    client.publish("home/quarto/sensor/PIR-1", "1");
-    //Serial.println("Tem alguem no quarto");
+  if (sensorVal == HIGH) {  
+    char _val[4]  = "";
+    sprintf(_val, "%d", sensorVal);
+    //digitalWrite(pinoLedQuarto, HIGH); 
+    client.publish_P("home/quarto/sensor/PIR-1", _val, true);
   } else {
-    digitalWrite(pinoLedQuarto, LOW);
+    //digitalWrite(pinoLedQuarto, LOW);
     //Serial.println("O quarto esta vazio");
   }
 }
+
+void verificaUltrasonico() {  
+  float distancia = ultrasonic.Ranging(CM); 
+  char _dist[20]  = "";
+  sprintf(_dist, "%.4f", distancia);
+  Serial.print("distancia.: ");
+  Serial.println(_dist);
+  delay(300);
+  
+  if (distancia < 30)
+  {
+    //digitalWrite(pinoLedAmarelo, HIGH);
+    client.publish_P("home/quarto/sensor/UIR-1", _dist, true);
+  } else {
+    //digitalWrite(pinoLedAmarelo, LOW);
+  }  
+}
+
+// void verificaIdosoDeitado(){
+//   int sensorVal = digitalRead(pinoButtonQuarto);  
+//   if (sensorVal == HIGH) {
+//     digitalWrite(pinoLedVermelho, LOW);
+//     Serial.println("O idoso esta deitado");
+//   } else {
+//     digitalWrite(pinoLedVermelho, HIGH);
+//     client.publish("home/quarto/sensor/BTN-1", "1");
+//     Serial.println("O idoso nao esta deitado");
+//   }
+// }
